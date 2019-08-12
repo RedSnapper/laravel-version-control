3c96d6a8796e67b9ee41769bdbb1995fdef02ebd
Param Dhaliwal <paramdhal@gmail.com>
2019-08-12 16:51:00 +0100

    Add user

---
+++ src/Models/User.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paramdeepdhaliwal
+ * Date: 2019-08-12
+ * Time: 16:46
+ */
+
+namespace Redsnapper\LaravelVersionControl\Models;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Auth\MustVerifyEmail;
+use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+
+class User extends BaseModel implements
+    AuthenticatableContract,
+    AuthorizableContract,
+    CanResetPasswordContract
+{
+    use Authenticatable, Authorizable, CanResetPassword, MustVerifyEmail;
+}
--- tests/Fixtures/Models/User.php
+++ tests/Fixtures/Models/User.php
@@ -2,43 +2,14 @@
 
 namespace Redsnapper\LaravelVersionControl\Tests\Fixtures\Models;
 
-use Exception;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Redsnapper\LaravelVersionControl\Models\BaseModel;
-use Redsnapper\LaravelVersionControl\Models\Traits\BelongsToRoles;
-use Illuminate\Notifications\DatabaseNotification;
-use Illuminate\Notifications\DatabaseNotificationCollection;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Auth\Authenticatable;
-use Illuminate\Auth\MustVerifyEmail;
-use Illuminate\Auth\Passwords\CanResetPassword;
-use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
-use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 
-/**
- * App\Models\User
- *
- * @property string $role_uid
- * @property string $username
- * @property string $email
- * @property string $emailp
- * @property string $password
- * @property string $active
- * @property string|null $remember_token
- * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
- */
-class User extends BaseModel implements
-    AuthenticatableContract,
-    AuthorizableContract,
-    CanResetPasswordContract
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Notifications\Notifiable;
+use Redsnapper\LaravelVersionControl\Models\User as Authenticatable;
+
+class User extends Authenticatable
 {
-    use Notifiable,
-        Authenticatable,
-        Authorizable,
-        CanResetPassword,
-        MustVerifyEmail;
+    use Notifiable;
 
     protected $fillable = ['role_uid','email','password'];
     protected $hidden = ['remember_token'];
